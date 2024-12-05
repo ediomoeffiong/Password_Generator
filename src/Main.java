508bfbd0a723cf86d598ea1d4b34fc50f952ecd8
Covenant <ediomoemmaeffiong@gmail.com>
2024-12-05 16:33:53 +0100

    Added some comments

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,7 @@ public class Main {
         System.out.print("Username/Email: ");
         String ue = scanDetails.nextLine();
 
+        // Provides symbols for alphabets (lower case and upper case), numbers, and special characters
         char[] str = {
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -35,18 +36,22 @@ public class Main {
                 '!', '@', '#', '$', '%', '^', '&', '*', '_',
         };
 
+        // Provides symbols for special characters
         char[] specialStr = {
                 '!', '@', '#', '$', '%', '^', '&', '*', '_',
         };
 
+        // Provides symbols for numbers
         char[] num = {
                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         };
 
+        // Provides symbols for lower case letters
         char[] lowerCase = {
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         };
 
+        // Provides symbols for upper case letters
         char[] upperCase = {
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         };
